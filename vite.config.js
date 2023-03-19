aa10b56073ed0b56db910769040fa32cb286908b
Quentin <quentingosset7500@hotmail.com>
2023-03-19 17:45:13 +0100

    issue-15: Used perk is wrong

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import copy from "rollup-plugin-copy";
+import zipPack from "vite-plugin-zip-pack";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,13 +13,15 @@ export default defineConfig({
       vue(),
       copy({
         targets: [
-            { src: 'dist/*', dest: 'extension_chrome/', overwrite: true },
-            { src: 'dist/*', dest: 'extension_firefox/', overwrite: true },
-            { src: 'manifest_chrome.json', dest: 'extension_chrome/',rename: 'manifest.json', overwrite: true },
-            { src: 'manifest_firefox.json', dest: 'extension_firefox/',rename: 'manifest.json', overwrite: true },
+            { src: 'dist/*', dest: 'extensions/extension_chrome/', overwrite: true },
+            { src: 'dist/*', dest: 'extensions/extension_firefox/', overwrite: true },
+            { src: 'manifest_chrome.json', dest: 'extensions/extension_chrome/',rename: 'manifest.json', overwrite: true },
+            { src: 'manifest_firefox.json', dest: 'extensions/extension_firefox/',rename: 'manifest.json', overwrite: true },
         ],
         overwrite: true
-      })
+      }),
+      zipPack({inDir: 'extension_chrome/', outDir: 'extensions', outFileName: 'extension_chrome.zip'}),
+      zipPack({inDir: 'extension_firefox/', outDir: 'extensions', outFileName: 'extension_firefox.zip'}),
   ],
   resolve: {
     alias: [
