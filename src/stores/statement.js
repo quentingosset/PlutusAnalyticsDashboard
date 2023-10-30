0f47a665d10d99ca6ba1c8788b477e1b2862203e
Quentin Gosset <quentingosset7500@hotmail.com>
2023-10-30 23:03:06 +0100

    Fix limits UI

--- src/stores/statement.js
+++ src/stores/statement.js
@@ -38,7 +38,7 @@ export const statementStore = defineStore({
                         && !StatementsType.REFUNDED.equals(value.type)
                         && !StatementsType.DECLINED.equals(value.type)
                     )}).reduce((sum, transaction) => {
-                        return sum + (transaction.amount / 100);
+                        return sum + (Math.abs(transaction.amount) / 100);
                     },0);
                 case Frequency.MONTHLY:
                     return this.statements.filter((value) => {return (dayjs(value.date).isBetween(dayjs().subtract(1, 'month'),dayjs(),null, '[]')
@@ -51,7 +51,7 @@ export const statementStore = defineStore({
                         && !StatementsType.REFUNDED.equals(value.type)
                         && !StatementsType.DECLINED.equals(value.type)
                     )}).reduce((sum, transaction) => {
-                        return sum + (transaction.amount / 100);
+                        return sum + (Math.abs(transaction.amount) / 100);
                     },0);
                 case Frequency.YEARLY:
                     return this.statements.filter((value) => {return (dayjs(value.date).isBetween(dayjs().subtract(1, 'year'),dayjs(),null, '[]')
@@ -64,21 +64,9 @@ export const statementStore = defineStore({
                         && !StatementsType.REFUNDED.equals(value.type)
                         && !StatementsType.DECLINED.equals(value.type)
                     )}).reduce((sum, transaction) => {
-                        return sum + (transaction.amount / 100);
+                        return sum + (Math.abs(transaction.amount) / 100);
                     },0);
                 case Frequency.YEARLY_CASHBACK:
-                    console.log(this.statements.filter((value) => {
-                    return (dayjs(value.date).isBetween(dayjs().subtract(1, 'year'),dayjs(),null, '[]')
-                        && !StatementsType.TOP_UP_ACCOUNT.equals(value.type)
-                        && !StatementsType.WITHDRAW_ACCOUNT_TO_CARD.equals(value.type)
-                        && !StatementsType.TOP_UP_CARD.equals(value.type)
-                        && !StatementsType.DEX_BUY.equals(value.type)
-                        && !StatementsType.WITHDRAW_FEE.equals(value.type)
-                        && !StatementsType.BONUS.equals(value.type)
-                        && !StatementsType.REFUNDED.equals(value.type)
-                        && !StatementsType.DECLINED.equals(value.type)
-                        && value.cashback.length === 0
-                    )}));
                     return this.statements.filter((value) => {
                         return (dayjs(value.date).isBetween(dayjs().subtract(1, 'year'),dayjs(),null, '[]')
                         && !StatementsType.TOP_UP_ACCOUNT.equals(value.type)
@@ -91,7 +79,7 @@ export const statementStore = defineStore({
                         && !StatementsType.DECLINED.equals(value.type)
                         && value.cashback.length !== 0
                     )}).reduce((sum, transaction) => {
-                        return sum + (transaction.amount / 100);
+                        return sum + (Math.abs(transaction.amount) / 100);
                     },0);
             }
         },
